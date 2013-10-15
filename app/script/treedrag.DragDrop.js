cc346271d367ef6b7fb536a51826b4967ee74897
dberseron <damien.berseron@me.com>
2013-10-15 19:01:20 +0200

    avancement sur la gestion du drag, c'est opérationnel. Il faut maintenant le rendre sexy

--- app/script/treedrag.DragDrop.js
+++ app/script/treedrag.DragDrop.js
@@ -6,115 +6,133 @@
  */
 
 var DragDrop = function () {
-  this.init.apply(this, arguments);
+	this.init.apply(this, arguments);
 };
 
 DragDrop.prototype = {
-  constructor: DragDrop.prototype.constructor,
-  options: {
-    limitToParent: true
-  },
+	constructor: DragDrop.prototype.constructor,
+	options: {
+		limitToParent: true
+	},
 
-  init: function (element, options) {
-    this.$element = $(element);
-    this.options = $.extend(true, {}, this.options, options);
+	init: function (element, options) {
+		this.$element = $(element);
+		this.options = $.extend(true, {}, this.options, options);
 
-    this.emptydroppables = this.$element.find('.empty-droppable');
+		this.items = this.$element.find('li');
+		this.emptyDroppables = this.$element.find('li.empty-droppable');
 
-    this.addEvents();
-  },
+		this.addEvents();
+	},
 
-  addEvents: function () {
-    var _this = this;
-    var elms = this.$element.find('li');
+	addEvents: function () {
+		var _this = this;
 
-    elms.drag('init', $.proxy(this.onDragInit, this));
-    elms.drag('start', $.proxy(this.onDragStart, this));
-    elms.drag($.proxy(this.onDrag, this));
-    elms.drag('end', $.proxy(this.onDragEnd, this));
+		this.items.drag('init', $.proxy(this.onDragInit, this));
+		this.items.drag('start', $.proxy(this.onDragStart, this));
+		this.items.drag($.proxy(this.onDrag, this));
+		this.items.drag('end', $.proxy(this.onDragEnd, this));
 
-    elms.drop('init', $.proxy(this.onDropInit, this));
-    /*elms.drop('start', $.proxy(this.onDropStart, this));
-    elms.drop($.proxy(this.onDrop, this));
-    elms.drop('end', $.proxy(this.onDropEnd, this));*/
+		this.items.drop('init', $.proxy(this.onDropInit, this));
+		this.items.drop('start', $.proxy(this.onDropStart, this));
+		this.items.drop($.proxy(this.onDrop, this));
+		this.items.drop('start', $.proxy(this.onDropEnd, this));
 
-    $.drop({
-      tolerance: function (event, proxy, target) {
-        var test = event.pageY > ( target.top + target.height / 2 );
-        $.data(target.elem, "drop+reorder", test ? "insertAfter" : "insertBefore");
-        return this.contains(target, [ event.pageX, event.pageY ]);
-      }
-    });
-  },
+		/*$.drop({
+		 tolerance: function (event, proxy, target) {
+		 var test = event.pageY > ( target.top + target.height / 2 );
+		 $.data(target.elem, "drop+reorder", test ? "insertAfter" : "insertBefore");
+		 return this.contains(target, [ event.pageX, event.pageY ]);
+		 }
+		 });*/
+	},
 
-  // =============
-  // DRAG methods
-  // ==============
-  onDragInit: function (ev, dd) {
-    var currentLevel = this.currentLevel = $(dd.target).data('level');
-    this.emptydroppables.filter(function() {
-      return $(this).data('level')==currentLevel;
-    }).show();
-  },
+	// =============
+	// DRAG methods
+	// ==============
+	onDragInit: function (ev, dd) {
+		var currentLevel = this.currentLevel = $(dd.drag).data('level');
+		var emptyTarget = this.emptyDroppables.filter(function () {
+			return $(this).data('level') == currentLevel;
+		});
+		emptyTarget.addClass('active');
+	},
 
-  onDragStart:function(ev, dd) {
-    var $elm = $(dd.drag);
-    this.createPhantom($elm);
-    this.setElemPos($elm, dd);
-    $elm.css('width', $elm.width());
-    $elm.addClass('treedrag-isdragging');
-    this.$element.append($elm);
-  },
+	onDragStart: function (ev, dd) {
+//		console.log('onDragStart')
+		var $elm = $(dd.drag);
+		this.createPhantom($elm);
+		this.setElemPos($elm, dd);
+		$elm.css('width', $elm.width());
+		$elm.addClass('treedrag-isdragging');
+		this.$element.append($elm);
+	},
 
-  onDrag: function (ev, dd) {
-    this.setElemPos($(dd.drag), dd);
+	onDrag: function (ev, dd) {
+//		console.log('onDrag')
+		this.setElemPos($(dd.drag), dd);
 
-    var drop = dd.drop[0],
-        method = $.data( drop || {}, "drop+reorder" );
-    if ( drop && ( drop != dd.current || method != dd.method ) ){
-      if(this.phantom) this.phantom[ method ]( drop );
-      dd.current = drop;
-      dd.method = method;
-      dd.update();
-    }
+		var drop = dd.drop[0],
+			method = $.data(drop || {}, "drop+reorder");
+		/*if ( drop && ( drop != dd.current || method != dd.method ) ){
+		 if(this.phantom) this.phantom[ method ]( drop );
+		 dd.current = drop;
+		 dd.method = method;
+		 dd.update();
+		 }*/
 
-   // console.dir(dd);
+		// console.dir(dd);
 
-  },
+	},
 
-  onDragEnd:function(ev, dd) {
-    this.phantom.replaceWith(dd.target);
-    $(dd.target).removeClass('treedrag-isdragging');
-  },
+	onDragEnd: function (ev, dd) {
+//		console.log('onDragEnd', dd.target, dd.drop[0]);
+		$(dd.target).css('width', '');
+		this.emptyDroppables.removeClass('active');
+		this.emptyDroppables.each(function(){
+			$(this).appendTo($(this).parent());
+		});
+		this.phantom.replaceWith(dd.target);
+		$(dd.target).removeClass('treedrag-isdragging');
 
-  // =============
-  // DROP methods
-  // ==============
-  onDropInit: function (ev, dd) {
-    return  !( dd.target == dd.drag) ;
-  },
-  onDropStart: function (ev, dd) {
-    console.log("dropstart");
-  },
-  onDrop: function (ev, dd) {
-    console.log("drop");
-  },
-  onDropEnd: function (ev, dd) {
-    console.log("dropend");
-  },
+		if(dd.drop[0]){
+			$(dd.target).insertBefore($(dd.drop[0]));
+		}
+	},
 
-  createPhantom:function(element) {
-    var $elm = $(element);
-    var phantom = this.phantom = $elm.clone();
-    phantom.addClass('treedrag-phantom');
-    $elm.after(phantom);
-  },
+	// =============
+	// DROP methods
+	// ==============
+	onDropInit: function (ev, dd) {
+		/*console.log("onDropInit", dd.target, dd.drag)*/
+		return  !( dd.target == dd.drag || $(dd.target).data('level') != $(dd.drag).data('level'));
+	},
+	onDropStart: function (ev, dd) {
+//		console.log('onDropStart');
+		var currentTarget = $(dd.target);
+		currentTarget.parent().find('> .empty-droppable').insertBefore(currentTarget);
+		currentTarget.parent().find('> .empty-droppable').addClass('dropHover');
+	},
+	onDropEnd: function (ev, dd) {
+//		console.log("dropEnd");
+		$(dd.target).parent().find('> .empty-droppable').removeClass('dropHover');
+	},
+	onDrop: function (ev, dd) {
+//		console.log("drop");
+	},
 
-  setElemPos : function(elm,dd) {
-    elm.css({
-      top: dd.offsetY,
-      left: dd.offsetX
-    })
-  }
+	createPhantom: function (element) {
+		var $elm = $(element);
+		var phantom = this.phantom = $elm.clone();
+		phantom.addClass('treedrag-phantom');
+		$elm.after(phantom);
+	},
+
+	setElemPos: function (elm, dd) {
+		elm.css({
+			top: dd.offsetY,
+			left: dd.offsetX
+		})
+	}
 };
 
